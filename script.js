ceb757f4557d6ad40be4df2ef893f6847b60b012
preisin <103018764+preisin@users.noreply.github.com>
2025-01-24 11:45:13 -0800

    Made togglable grid

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded",function() {
-	console.log("JS is running!");
+function handleButtonClick(index, button) {
+    // Mark the button as filled or unfilled
+    if (button.style.backgroundColor === 'black') {
+        button.style.backgroundColor = 'white';
+    } else {
+        button.style.backgroundColor = 'black';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const board = document.getElementById("sudoku-board");
+
+    for (let i = 0; i < 81; i++) {
+        let button = document.createElement("button");
+		button.style.backgroundColor = 'white';
+        button.addEventListener('click', function () {
+            handleButtonClick(i, button); // Pass the button index and button element
+        });
+
+        board.appendChild(button);
+    }
 });
