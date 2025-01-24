2b625ba961c8b664cf308477c5aac3ea9b74b146
preisin <103018764+preisin@users.noreply.github.com>
2025-01-24 14:19:57 -0800

    Made game work

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function handleButtonClick(index, button) {
-    // Mark the button as filled or unfilled
+function flip(button) {
     if (button.style.backgroundColor === 'black') {
         button.style.backgroundColor = 'white';
     } else {
@@ -7,16 +6,26 @@ function handleButtonClick(index, button) {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const board = document.getElementById("sudoku-board");
+function handleButtonClick(index, board) {
+    flip(board.childNodes[index]);
+    if (index % 6 > 0) flip(board.childNodes[index-1]);
+    if (index % 6 < 5) flip(board.childNodes[index+1]);
+    if (Math.floor(index / 6) > 0) flip(board.childNodes[index-6]);
+    if (Math.floor(index / 6) < 5) flip(board.childNodes[index+6]);
+}
 
-    for (let i = 0; i < 81; i++) {
+document.addEventListener("DOMContentLoaded", function() {
+    const board = document.getElementById("board");
+
+    for (let i = 0; i < 36; i++) {
         let button = document.createElement("button");
 		button.style.backgroundColor = 'white';
         button.addEventListener('click', function () {
-            handleButtonClick(i, button); // Pass the button index and button element
+            handleButtonClick(i, board); // Pass the button index and button element
         });
 
         board.appendChild(button);
     }
+    flip(board.childNodes[20]);
+    flip(board.childNodes[25]);
 });
