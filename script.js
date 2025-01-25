a0733a8eeb55b4ec70dfd2f066f254fbf84c9d98
preisin <103018764+preisin@users.noreply.github.com>
2025-01-25 11:18:27 -0800

    Made solver

--- script.js
+++ script.js
@@ -14,18 +14,55 @@ function handleButtonClick(index, board) {
     if (Math.floor(index / 6) < 5) flip(board.childNodes[index+6]);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+function empty(board) {
+    buttons = board.childNodes;
+    for (let i = 0; i < 36; i++) {
+        if (buttons[i].style.backgroundColor === "black") {
+            return false;
+        }
+    }
+    return true;
+}
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function handleSolveClick(index, board) {
+    if (index == 36) {
+        return empty(board);
+    }
+
+    await wait(1);
+
+    handleButtonClick(index,board);
+    for (let i = index+1; i <= 36; i++) {
+        let done = await handleSolveClick(i, board);
+        if (done) {
+            return true;
+        }
+    }
+    handleButtonClick(index,board);
+    return false;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
     const board = document.getElementById("board");
 
     for (let i = 0; i < 36; i++) {
-        let button = document.createElement("button");
+        let button = document.createElement('button');
 		button.style.backgroundColor = 'white';
         button.addEventListener('click', function () {
-            handleButtonClick(i, board); // Pass the button index and button element
+            handleButtonClick(i, board);
         });
 
         board.appendChild(button);
     }
     flip(board.childNodes[20]);
     flip(board.childNodes[25]);
+
+    const solve = document.getElementById("solve");
+    solve.addEventListener('click', function () {
+        handleSolveClick(0, board);
+    });
 });
