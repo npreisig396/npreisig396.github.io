829de1ac42b6272379e8a20aab0b630ed804ade5
npreisig <npreisig396@gmail.com>
2025-09-17 12:53:14 -0700

    Added descriptions

--- education/education.js
+++ education/education.js
@@ -6,15 +6,16 @@ fetch('education/education.html').then(r => r.text()).then(html => {
 
     const education = [
         { title: 'Computer Science M.S. - Western Washington University', asset: 'assets/education/wwu.png',
-            years: '2024-2026' },
+            years: '2024-2026', description: 'Completing a Master\'s of Science in Computer Science at Western Washington University with a focus in machine learning research under Dr. Brian Hutchinson. Developed machine learning solutions to real world problems in bioinformatics.'},
         { title: 'Computer Science B.S. - Western Washington University', asset: 'assets/education/wwu.png',
-            years: '2020-2024' },
+            years: '2020-2024', description: 'Completed a Bachelor\'s of Science in Computer Science at Western Washington University with minors in both Mathematics and Physics. Topics covered include data structures, analysis of algorithms, operating systems, databases, and machine learning. Languages covered include Python, Java, C, Go, Racket, and mySQL.' },
     ]
 
     education.forEach(education => {
         const clone = template.content.cloneNode(true);
         clone.querySelector('.education-title').textContent = education.title;
         clone.querySelector('.education-subtitle').textContent = education.years;
+        clone.querySelector('.education-description').textContent = education.description;
         const mediaContainer = clone.querySelector('.education-media');
         if (education.asset.endsWith('.png')) {
             const img = document.createElement('img');
